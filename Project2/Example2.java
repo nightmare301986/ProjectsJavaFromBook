5f3fc7cf54aa9933ab67805d1e8fa243c544bbf3
Пахомов Евгений <nightmare23@yandex.ru>
2023-03-03 00:00:16 +0700

    Initial commit

---
+++ Project2/Example2.java
@@ -0,0 +1,13 @@
+package Project2;
+
+public class Example2 {
+    public static void main ( String [] args) { 
+        int num; // объявление переменной по имени nurn 
+        num = 100; // присваивание переменной nurn значения 100 
+        System.out . println ( "Значение пum: " + num) ; 
+        
+        num = num * 2; 
+        System.out.print ( "Знaчeниe num * 2: " ) ; 
+        System.out.println (num) ;
+    }
+}
